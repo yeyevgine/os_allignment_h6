79c2fe8d12a96bf6aa31370144544ca1193832eb
yevmnat <evgine.v.mnatsakanyan@gmail.com>
2025-10-18 15:54:07 +0400

    Update assignment1.c

--- assignment1.c
+++ assignment1.c
@@ -8,18 +8,15 @@ int main()
 	double c;
 	short d;
 
-
 	printf("Size of int: %zu bytes\n", sizeof(a));
-        printf("Size of char: %zu bytes\n", sizeof(b));
-        printf("Size of double: %zu bytes\n", sizeof(c));
-        printf("Size of short: %zu bytes\n", sizeof(d));
-
+    printf("Size of char: %zu bytes\n", sizeof(b));
+    printf("Size of double: %zu bytes\n", sizeof(c));
+    printf("Size of short: %zu bytes\n", sizeof(d));
 
 	printf("Address of int a: %p\n", &a);        
 	printf("Address of char b: %p\n", &b);
 	printf("Address of double c: %p\n", &c);
 	printf("Address of short d: %p\n", &d);
-
-
+	
 	return 0;
 }
